2f86685640f2df958246b1db9b645a3812ee7664
Huy Nguyen <112622004+npxhuy@users.noreply.github.com>
2023-03-08 01:30:57 +0100

    Update Untitled.R

--- Data/Untitled.R
+++ Data/Untitled.R
@@ -1,9 +1,9 @@
 setwd("~/Documents/GitHub/popgen_binp29/Data")
-install.packages("readxl")
-install.packages("dplyr")
-library("dplyr")
-library("readxl")
-
+# install.packages("readxl")
+# install.packages("dplyr")
+# library("dplyr")
+# library("readxl")
+rm(list=ls())
 # Load the excel file (main data)
 maindata <- read_excel("V50.xlsx")
 
@@ -19,16 +19,21 @@ colnames(famdata) <- c("Country","ID","FatherID","MotherID","Sex","Phenotype")
 filter2_maindata = unique(filter1_maindata[filter1_maindata$`Master ID` %in%  famdata$ID,])
 colnames(filter2_maindata) <- c("MasterID","DateMean")
 
-# Sort maindata
-sort_maindata <- filter2_maindata[order(filter2_maindata$DateMean),]
-
-
-data1 <-sort_maindata[sort_maindata$DateMean>10000,]
 # Seperate data by time, each step is 500
 end_time=(max(filter2_maindata$DateMean))
 start_time=min(filter2_maindata$DateMean)
 time_step=500
-timeseq=seq(start_time,end_time+500,time_step)
+timeseq0=seq(start_time-1,end_time+500,time_step)
+
+# Seperate timeseq into 3 different time step
+timeseq1=timeseq0[timeseq0<=8000]
+timeseq2_1=timeseq0[timeseq0>8000 & timeseq0<=11000]
+timeseq2_2=timeseq2[c(FALSE,TRUE)] # Time step = 1000
+timeseq3=max(timeseq0) # Take the rest
+
+# Overall timeseq
+timeseq=c(timeseq1,timeseq2_2,timeseq3)
+
 
 # Generate different data base on different time step
 for (i in 1:(length(timeseq)-1)) { 
@@ -36,9 +41,10 @@ for (i in 1:(length(timeseq)-1)) {
   # gsub remove the space in the variable name
   # assign assign the variable name with the data 
   # data was filter base on the timeseq
-  assign(gsub(" ","",paste("data_",as.character(i))),filter2_maindata[filter2_maindata$DateMean>=timeseq[i] & filter2_maindata$DateMean < timeseq[i+1], ])
+  assign(gsub(" ","",paste("data_",as.character(i))),
+         filter2_maindata[filter2_maindata$DateMean>=timeseq[i] & filter2_maindata$DateMean < timeseq[i+1], ])
 }
 
-rm(list=ls())
+
 
 
